19c71abc40f7c8fd2f52ed66a86b9301efa57454
Opanpan <fanalriansyah@gmail.com>
2021-12-19 03:09:29 +0700

    create route for get user's item

--- routers/item.js
+++ routers/item.js
@@ -4,5 +4,6 @@ const item = express.Router();
 const itemController = require("../controllers/itemControllers");
 
 item.post("/create", itemController.addItemAction);
+item.get("/", itemController.getUserItemAction);
 
 module.exports = item;
