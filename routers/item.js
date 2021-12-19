a91d7e7e04412369202df79ca18723d5372653bb
Opanpan <fanalriansyah@gmail.com>
2021-12-19 13:43:17 +0700

    create controller for delete item

--- routers/item.js
+++ routers/item.js
@@ -5,5 +5,6 @@ const itemController = require("../controllers/itemControllers");
 
 item.post("/create", itemController.addItemAction);
 item.get("/", itemController.getUserItemAction);
+item.post("/delete", itemController.removeItemAction);
 
 module.exports = item;
